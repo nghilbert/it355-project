0c34f4440a628df3b73c490b8dad514ffbfd7651
Nate <nathanghilbert@gmail.com>
2026-03-04 13:22:09 -0600

    added more output

--- rules/nate/ThreadTask.java
+++ rules/nate/ThreadTask.java
@@ -8,7 +8,7 @@ public class ThreadTask implements Runnable {
 	/** Prints the name of the thead that is executing the task */
 	@Override
 	public void run() {
-		System.out.println("I am a thread running this task. My name is: " + Thread.currentThread().getName());
+		System.out.println("I am a thread running this task. My ID: " + Thread.currentThread().getName());
 	}
 
 	/**
@@ -22,8 +22,11 @@ public class ThreadTask implements Runnable {
 		thread.start();
 	}
 
-	/** Run a test case */
+	/** Run 3 threads to see if they have different IDs */
 	public static void main(String[] args) {
-		startNewThread(new ThreadTask());
+		ThreadTask threadTask = new ThreadTask();
+		startNewThread(threadTask);
+		startNewThread(threadTask);
+		startNewThread(threadTask);
 	}
 }
