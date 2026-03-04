612d2dc53a8c7014486fe1c477ed9634d2be676f
Nate <nathanghilbert@gmail.com>
2026-03-04 13:15:06 -0600

    made ThreadTask  more compliant by adding a function

--- rules/nate/ThreadTask.java
+++ rules/nate/ThreadTask.java
@@ -11,15 +11,19 @@ public class ThreadTask implements Runnable {
 		System.out.println("I am a thread running this task. My name is: " + Thread.currentThread().getName());
 	}
 
-	public static void main(String[] args) {
-		ThreadTask task = new ThreadTask();
-
-		/**
-		 * Do not call run() directly on the thread. This will run the action on the
-		 * current thread instead of creating a new one. Use start() to create a new
-		 * thread and execute the task.
-		 */
+	/**
+	 * Use this function to create a new thread so someone doesn't accidentally use
+	 * run(). Calling run() directly on the thread will run the task on the
+	 * current thread instead of creating a new one. Use start() to create a new
+	 * thread and execute the task.
+	 */
+	public static void startNewThread(ThreadTask task) {
 		Thread thread = new Thread(task);
 		thread.start();
+	}
+
+	/** Run a test case */
+	public static void main(String[] args) {
+		startNewThread(new ThreadTask());
 	}
 }
